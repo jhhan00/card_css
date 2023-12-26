488ead43ad12d2fa9ea4ad4d85dad33f10cf09b0
jhhan00 <jhhan009@gmail.com>
2023-12-26 22:40:46 +0900

    3d 회전처럼 보이도록 css 추가

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 console.log('css coding~');
 
 var container = document.querySelector(".container");
-console.log(container);
+container.addEventListener("mousemove", function(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    const rotateX = y * 4 / 30 - 20;
+    const rotateY = -x / 5 + 20;
+
+    container.style = `transform : perspective(300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
