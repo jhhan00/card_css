f43fc9e00156026ae3651fcd3281696c4e7a29f3
jhhan00 <jhhan009@gmail.com>
2023-12-27 22:36:21 +0900

    빛나는 효과 추가

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
 console.log('css coding~');
 
-var container = document.querySelector(".container");
-container.addEventListener("mousemove", function(e) {
+var container1 = document.querySelector(".container1");
+container1.addEventListener("mousemove", function(e) {
     const x = e.offsetX;
     const y = e.offsetY;
     const rotateX = y * 4 / 30 - 20;
     const rotateY = -x / 5 + 20;
 
-    container.style = `transform : perspective(300px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    container1.style = `transform : perspective(300px)
+        rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
 });
 
-container.addEventListener("mouseleave", function() {
-    container.style = "";
+container1.addEventListener("mouseout", function() {
+    container1.style = `transform: perspective(300px)
+        rotateX(0deg) rotateY(0deg)`;
+});
+
+var overlay1 = document.querySelector(".overlay1");
+overlay1.addEventListener("mousemove", function(e) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    overlay1.style = `background-position: ${x/5 + y/6}%`;
+});
+
+overlay1.addEventListener("mouseout", function() {
+    overlay1.style = `filter: opacity(0)`;
 });
--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@
 
 ##### 참조: https://www.youtube.com/watch?v=YDCCauu4lIk
 ##### 사진참조: https://tcgbox.co.kr/product/%EB%A6%AC%EC%9E%90%EB%AA%BDv-scr-001/11522/
+https://tcgbox.co.kr/product/%ED%95%AB%EC%82%BC-vmax-s3-067/11712/
 
 1. 카드를 회전할 수 있는 css 적용
 2. 카드에 빛나는 효과를 줄 수 있는 css 적용
